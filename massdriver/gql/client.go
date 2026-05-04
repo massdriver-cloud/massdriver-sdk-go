249ee5a57e5de443ffd69ce80166c5ca1fc122f8
chrisghill <chris@massdriver.cloud>
2026-05-04 16:50:21 -0600

    Update V1 -> V2 GQL API

--- massdriver/gql/client.go
+++ massdriver/gql/client.go
@@ -7,8 +7,7 @@ import (
 	"github.com/massdriver-cloud/massdriver-sdk-go/massdriver/config"
 )
 
-const gqlV0Path = "/api"
-const gqlV1Path = "/api/v1"
+const gqlV2Path = "/api/v2"
 
 type RoundTripperWithHeaders struct {
 	Headers map[string]string
@@ -22,23 +21,8 @@ func (r *RoundTripperWithHeaders) RoundTrip(req *http.Request) (*http.Response, 
 	return r.Base.RoundTrip(req)
 }
 
-func NewV0Client(config *config.Config) graphql.Client {
-	baseURL := config.URL + gqlV0Path
-
-	transport := &RoundTripperWithHeaders{
-		Base: http.DefaultTransport,
-		Headers: map[string]string{
-			"Authorization": config.Credentials.AuthHeaderValue,
-			"Content-Type":  "application/json",
-		},
-	}
-
-	httpClient := &http.Client{Transport: transport}
-	return graphql.NewClient(baseURL, httpClient)
-}
-
-func NewV1Client(config *config.Config) graphql.Client {
-	baseURL := config.URL + gqlV1Path
+func NewV2Client(config *config.Config) graphql.Client {
+	baseURL := config.URL + gqlV2Path
 
 	transport := &RoundTripperWithHeaders{
 		Base: http.DefaultTransport,
